9a85082bb563ab33ceff27495e42f504a441f666
Oscar Wong <wong.oscar67@gmail.com>
2022-03-11 12:43:00 -0700

    Mongodb setup

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import clientPromise from '../../lib/mongodb'
+
+export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = await client.db();
+  // this line is pretty gross but it's just meant to be an example
+  const testResult = (await db.collection('scores').find({}).toArray())[0];
+
+  res.status(200).json({ name: testResult.name })
 }
