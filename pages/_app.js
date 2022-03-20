b94811c0eb2d2fb01a46b5d8a1a1e99b2febca8c
Oscar Wong <wong.oscar67@gmail.com>
2022-03-19 19:36:21 -0600

    Dark theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import '../styles/globals.css'
-// import ThemeProvider from "@material-ui/core/styles/ThemeProvider";
+import CssBaseline from "@mui/material/CssBaseline";
 import { createTheme, ThemeProvider } from "@mui/material";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={createTheme({palette: {mode: 'dark'}})}>
+    <ThemeProvider theme={createTheme({ palette: { mode: "dark" } })}>
+      <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
   );
