a7f621f62acfb296da8ae03c15aa7cec1de92f70
Antonio Santos <anton.jaime15@gmail.com>
2022-04-07 15:11:32 -0600

    minor bug

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {session && (
-        <AppBar position="static">
+        <AppBar position="static" >
           <Toolbar>
             <Box display="flex" flexGrow={1}>
               {/* Left Side */}
@@ -109,7 +109,7 @@ export default function Home() {
         <Game />
         <Login disableBackdropClick />
       </main>
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -120,7 +120,7 @@ export default function Home() {
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 }
